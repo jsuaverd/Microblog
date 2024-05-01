6d418f1b558074d0e7734e277c9178df78e8e54c
jsuaverd <jsuaverd@my.centennialcollege.ca>
2024-05-01 11:14:24 -0400

    database

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,19 @@
 from flask import Flask
 from config import Config
 
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+
 
 app = Flask(__name__)
 app.config.from_object(Config)
 
 
-from app import routes
+#crating an instance of SQLALchemy class
+db = SQLAlchemy(app)
+
+#creating a migration object that associates app and db
+migrate = Migrate(app,db)
+
+
+from app import routes, models
