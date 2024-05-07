934e7f1bca80511592af01a9b593862f794137e8
jsuaverd <jsuaverd@my.centennialcollege.ca>
2024-05-07 09:47:58 -0400

    Profile page and avatars

--- app/__init__.py
+++ app/__init__.py
@@ -4,9 +4,14 @@ from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 
+from flask_login import LoginManager
+
+
 
 app = Flask(__name__)
 app.config.from_object(Config)
+login = LoginManager(app)
+login.login_view = 'login'
 
 
 #crating an instance of SQLALchemy class
